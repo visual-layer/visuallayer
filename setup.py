b404c0c5248bff06761dc866d498551cb0e921fa
dnth <dickson.neoh@gmail.com>
2023-05-03 14:51:56 +0800

    update setup

--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
 from setuptools import setup, find_packages
 
+with open("./README.md") as f:
+    long_description = f.read()
+
 setup(
     name='vl_datasets',
-    version='0.0.1',
+    version='0.0.3',
     url='https://github.com/visual-layer/vl-datasets',
     author='Visual Layer',
     author_email='your-email@example.com',
     description='Clean datasets for computer vision.',
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
     packages=find_packages(),
     install_requires=[
         "torch",
-        "torchvision",
-        "pandas"
+        "torchvision"
     ]
 )
