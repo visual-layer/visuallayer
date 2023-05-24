1a59b129e739105ce8542ba54f214c19bc12864b
Dickson Neoh <dickson.neoh@gmail.com>
2023-05-24 11:36:28 +0800

    Add test for imports for Ubuntu and MacOS (#19)
    * update
    
    * add smoke test
    
    * update python script
    
    * update path
    
    * add os
    
    * add windows and version print
    
    * only run on pr
    
    * opt out sentry
    
    * remove windoes

---
+++ .github/workflows/test_imports.py
@@ -0,0 +1,6 @@
+import vl_datasets
+from vl_datasets.vl_food101 import VLFood101
+from vl_datasets.vl_oxford_iiit_pet import VLOxfordIIITPet
+
+print(f'Running on versio: {vl_datasets.__version__}')
+print("Successfully imported all modules")
