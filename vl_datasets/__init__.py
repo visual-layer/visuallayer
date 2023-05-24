6cf6b8898b664f1f87e7d72500b529ce5f02f6ef
dnth <dickson.neoh@gmail.com>
2023-05-24 10:34:48 +0800

    update

--- vl_datasets/__init__.py
+++ vl_datasets/__init__.py
@@ -1,6 +1,7 @@
 __version__ = '0.0.9'
+from .sentry import init_sentry
 init_sentry()
+
 from .image_folder import CleanImageFolder
 from .vl_food101 import VLFood101
 from .vl_oxford_iiit_pet import VLOxfordIIITPet
-from .sentry import init_sentry
