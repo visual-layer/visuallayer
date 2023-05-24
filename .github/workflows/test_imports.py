f497091b52957041c70f320b76867b07544e2d15
Dickson Neoh <dickson.neoh@gmail.com>
2023-05-24 12:42:02 +0800

    Update test_imports.py

--- .github/workflows/test_imports.py
+++ .github/workflows/test_imports.py
@@ -2,5 +2,5 @@ import vl_datasets
 from vl_datasets.vl_food101 import VLFood101
 from vl_datasets.vl_oxford_iiit_pet import VLOxfordIIITPet
 
-print(f'Running on versio: {vl_datasets.__version__}')
+print(f'Running on version: {vl_datasets.__version__}')
 print("Successfully imported all modules")
