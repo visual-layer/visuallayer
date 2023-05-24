99c908eaedde40cc557bde4d7abd0fa3ef6333b2
Dickson Neoh <dickson.neoh@gmail.com>
2023-05-24 17:28:00 +0800

    Clean up redundant code. (#20)
    * update fastai nb with itable
    
    * update
    
    * refactor pets
    
    * add windows badge
    
    * refactor and rerun notebooks

--- vl_datasets/vl_food101.py
+++ vl_datasets/vl_food101.py
@@ -1,8 +1,7 @@
-# Code adapted from https://github.com/pytorch/vision/blob/main/torchvision/datasets/utils.py
+# Code adapted from https://github.com/pytorch/vision/blob/main/torchvision/datasets/
 
 from torchvision.datasets import Food101
-from typing import Any, Callable, Optional, TypeVar, Iterable
-from pathlib import Path
+from typing import Callable, Optional
 import json
 import pandas as pd
 import requests
@@ -38,20 +37,18 @@ class VLFood101(Food101):
         transform: Optional[Callable] = None,
         target_transform: Optional[Callable] = None,
         download: bool = True,
-        exclude_csv: Optional[str] = None
+        exclude_csv: Optional[str] = None,
     ) -> None:
         vl_capture_log_debug_state(locals())
-        super().__init__(root, transform=transform, target_transform=target_transform, download=download)
-        self._split = verify_str_arg(split, "split", ("train", "test"))
-        self._base_folder = Path(self.root) / "food-101"
-        self._meta_folder = self._base_folder / "meta"
-        self._images_folder = self._base_folder / "images"
 
-        if not self._check_exists():
-            raise RuntimeError("Dataset not found. You can use download=True to download it")
+        super().__init__(
+            root,
+            split=split,
+            transform=transform,
+            target_transform=target_transform,
+            download=download,
+        )
 
-        self._labels = []
-        self._image_files = []
         with open(self._meta_folder / f"{split}.json") as f:
             metadata = json.loads(f.read())
 
@@ -60,10 +57,10 @@ class VLFood101(Food101):
 
         # Default tranform
         if transform is None:
-            if split == 'train':
+            if split == "train":
                 self.transform = train_transform
 
-            elif split == 'test':
+            elif split == "test":
                 self.transform = valid_transform
 
         # If user specifies a csv file use it
@@ -71,11 +68,11 @@ class VLFood101(Food101):
             print(f"Using provided CSV file: {exclude_csv}")
             self.exclude_df = pd.read_csv(exclude_csv, header=0)
             self.exclude_set = set(self.exclude_df["filename"].tolist())
-        
+
         # Otherwise, download the csv file
         elif exclude_csv is None:
             print("Downloading CSV file")
-            url = "https://drive.google.com/uc?export=download&id=1QaaNJif3qWC_AsOEugnnBASySZO0cLjW" 
+            url = "https://drive.google.com/uc?export=download&id=1QaaNJif3qWC_AsOEugnnBASySZO0cLjW"
             filename = "food-101.csv"
 
             try:
@@ -93,50 +90,20 @@ class VLFood101(Food101):
 
         # A copy of self.exclude_set but without file extension
         exclude_set_filenames = {
-            filename.split("/")[-2] + "/" + filename.split("/")[-1].split(".")[0] for filename in self.exclude_set
+            filename.split("/")[-2] + "/" + filename.split("/")[-1].split(".")[0]
+            for filename in self.exclude_set
         }
 
+        self._labels = []
+        self._image_files = []
         self.excluded_files = []
         for class_label, im_rel_paths in metadata.items():
             for im_rel_path in im_rel_paths:
                 if f"{im_rel_path}" in exclude_set_filenames:
-                    print(f"Excluding {im_rel_path}.jpg")
+                    print(f"Excluding {im_rel_path}.jpg from the {split} set")
                     self.excluded_files.append(f"{im_rel_path}.jpg")
                     continue
                 self._labels += [self.class_to_idx[class_label]]
-                self._image_files += [self._images_folder.joinpath(*f"{im_rel_path}.jpg".split("/"))]
-
-        print(f"Excluded {len(self.excluded_files)} from the {split} set")
-
-
-T = TypeVar("T", str, bytes)
-
-def iterable_to_str(iterable: Iterable) -> str:
-    return "'" + "', '".join([str(item) for item in iterable]) + "'"
-
-def verify_str_arg(
-    value: T,
-    arg: Optional[str] = None,
-    valid_values: Optional[Iterable[T]] = None,
-    custom_msg: Optional[str] = None,
-) -> T:
-    if not isinstance(value, str):
-        if arg is None:
-            msg = "Expected type str, but got type {type}."
-        else:
-            msg = "Expected type str for argument {arg}, but got type {type}."
-        msg = msg.format(type=type(value), arg=arg)
-        raise ValueError(msg)
-
-    if valid_values is None:
-        return value
-
-    if value not in valid_values:
-        if custom_msg is not None:
-            msg = custom_msg
-        else:
-            msg = "Unknown value '{value}' for argument {arg}. Valid values are {{{valid_values}}}."
-            msg = msg.format(value=value, arg=arg, valid_values=iterable_to_str(valid_values))
-        raise ValueError(msg)
-
-    return 
+                self._image_files += [
+                    self._images_folder.joinpath(*f"{im_rel_path}.jpg".split("/"))
+                ]
--- vl_datasets/vl_oxford_iiit_pet.py
+++ vl_datasets/vl_oxford_iiit_pet.py
@@ -1,13 +1,12 @@
+# Code adapted from https://github.com/pytorch/vision/blob/main/torchvision/datasets/
+
 from torchvision.datasets import OxfordIIITPet
-import pathlib
 from typing import Callable, Optional, Union, Sequence
-from .utils import verify_str_arg
 from torchvision.datasets.vision import StandardTransform
 import pandas as pd
 import requests
 import torchvision.transforms as transforms
 from vl_datasets.sentry import v1_sentry_handler, vl_capture_log_debug_state
-
 
 
 train_transform = transforms.Compose(
@@ -28,6 +27,7 @@ valid_transform = transforms.Compose(
     ]
 )
 
+
 class VLOxfordIIITPet(OxfordIIITPet):
     @v1_sentry_handler
     def __init__(
@@ -42,40 +42,21 @@ class VLOxfordIIITPet(OxfordIIITPet):
         download: bool = True,
     ):
         vl_capture_log_debug_state(locals())
-        self._split = verify_str_arg(split, "split", ("trainval", "test"))
-        if isinstance(target_types, str):
-            target_types = [target_types]
-        self._target_types = [
-            verify_str_arg(target_type, "target_types", self._VALID_TARGET_TYPES)
-            for target_type in target_types
-        ]
 
         super().__init__(
-            root,
+            root=root,
             transforms=transforms,
             transform=transform,
             target_transform=target_transform,
-            download=download
+            download=download,
         )
 
-        self._base_folder = pathlib.Path(self.root) / "oxford-iiit-pet"
-        self._images_folder = self._base_folder / "images"
-        self._anns_folder = self._base_folder / "annotations"
-        self._segs_folder = self._anns_folder / "trimaps"
-
-        if download:
-            self._download()
-
-        if not self._check_exists():
-            raise RuntimeError(
-                "Dataset not found. You can use download=True to download it"
-            )
-
-        # # Default tranform
+        # Default tranform
         if transform is None:
             if split == "trainval":
                 print("Apply default transform")
                 self.transform = train_transform
+                print(self.transform)
 
             elif split == "test":
                 self.transform = valid_transform
@@ -107,8 +88,10 @@ class VLOxfordIIITPet(OxfordIIITPet):
 
         image_ids = []
         self._labels = []
+        self.excluded_files = []
 
         # A copy of self.exclude_set but without file extension
+        # oxford-iiit-pet/images/Abyssinian_177.jpg -> Abyssinian_177
         exclude_set_filenames = {
             filename.split("/")[-1].split(".")[0] for filename in self.exclude_set
         }
@@ -120,6 +103,7 @@ class VLOxfordIIITPet(OxfordIIITPet):
                 # if filename found in exclude set continue
                 if image_id in exclude_set_filenames:
                     print(f"Excluded {image_id} from the {split} set")
+                    self.excluded_files.append(f"{image_id}")
                     continue
 
                 image_ids.append(image_id)
