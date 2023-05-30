92a2b2cbe7181be3f53e2357c065fd3781bfc548
dnth <dickson.neoh@gmail.com>
2023-05-30 04:23:49 +0000

    add class

--- vl_datasets/vl_imagenet.py
+++ vl_datasets/vl_imagenet.py
@@ -1,7 +1,7 @@
 # Code adapted from https://github.com/pytorch/vision/blob/main/torchvision/datasets/
 
 from torchvision.datasets import ImageNet
-from typing import Callable, Optional, Union, Sequence, Any
+from typing import Callable, Optional, Union, Sequence, Any, Tuple
 import pandas as pd
 
 class VLImageNet(ImageNet):
@@ -11,9 +11,31 @@ class VLImageNet(ImageNet):
                  **kwargs: Any) -> None:
         
 
-        super().__init__(root=root, split=split, **kwargs)
+        super().__init__(root=root, split=split **kwargs)
+        
+        self.exclude_df, self.exclude_set = parse_exclude_csv(exclude_csv)
 
-        if exclude_csv:
-            print(f"Using provided CSV file: {exclude_csv}")
-            self.exclude_df = pd.read_csv(exclude_csv, header=0)
-            self.exclude_set = set(self.exclude_df["filename"].tolist())
+        # Filter file lists based on VL CSV files
+        # TODO: use more efficient method. This takes too long. Sets subtraction maybe?
+        image_keep_list = [i for i, (filename, class_num) in enumerate(self.samples) if not filename.endswith(tuple(self.exclude_set))]
+
+        self.samples = [self.samples[i] for i in image_keep_list]
+        self.targets = [self.targets[i] for i in image_keep_list]
+        
+
+def parse_exclude_csv(exclude_csv_arg: str) -> Tuple[pd.DataFrame, set]:
+    
+    if exclude_csv_arg:
+        print(f"Using provided CSV file: {exclude_csv_arg}")
+        exclude_df = pd.read_csv(exclude_csv_arg, header=0)
+        # Strip additional dir name and only use filenames.
+        exclude_df['filename'] = exclude_df['filename'].str.replace('imagenet1k/', '')
+        exclude_df['prototype'] = exclude_df['prototype'].str.replace('imagenet1k/', '')
+        exclude_set = set(exclude_df["filename"].tolist())
+
+    else:
+        print("CSV not provided, using original version of ImageNet")
+        exclude_df = pd.DataFrame()
+        exclude_set = set()
+
+    return exclude_df, exclude_set
