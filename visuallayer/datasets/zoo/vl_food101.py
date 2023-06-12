bbee4a5495f48cc0b7dead90e732d6ecf9a2efc5
dnth <dickson.neoh@gmail.com>
2023-06-12 11:05:53 +0800

    refactor dataset class

--- visuallayer/datasets/zoo/vl_food101.py
+++ visuallayer/datasets/zoo/vl_food101.py
@@ -6,11 +6,11 @@ from torchvision.datasets import Food101
 from typing import Union, List, Tuple
 
 @dataclass(frozen=True)
-class VLFood101:
+class VLFood101(Dataset):
     name: str = "vl-food101"
     homepage_url: str = "https://data.vision.ee.ethz.ch/cvl/datasets_extra/food-101/"
     license: str = "Unknown"
-    description: str = "A challenging data set of 101 food categories is introduced, consisting of 101,000 images."
+    description: str = "A modified version of the original Food101 Dataset removing dataset issues."
     num_images: int = 101000
     filelist_csv_url: str = "https://sharedvisuallayer.s3.us-east-2.amazonaws.com/visual-layer-sdk/food101_images_issue_file_list.csv"
     issue_count_csv_url: str = "https://sharedvisuallayer.s3.us-east-2.amazonaws.com/visual-layer-sdk/food101_images_issue_count.csv"
@@ -19,76 +19,6 @@ class VLFood101:
     # Hack: Download the dataset in the current dir
     def __post_init__(self):
         Food101(root="./", download=True)
-
-    @property
-    def num_images_with_issues(self) -> int:
-        df = pd.read_csv(self.filelist_csv_url)
-        return len(df["filename"].unique())
-    
-    @property
-    def info(self) -> None:
-        # Get all attributes and methods of the class
-        dataset_metadata: List[Tuple[str, Union[str, int]]] = [
-            ("Name", self.name),
-            ("Description", self.description),
-            ("License", self.license),
-            ("Homepage URL", self.homepage_url),
-            ("Number of Images", self.num_images),
-            ("Number of Images with Issues", self.num_images_with_issues),
-        ]
-
-        print("Metadata:")
-        for metadata in dataset_metadata:
-            print(f"--> {metadata[0]} - {metadata[1]}")
-    
-    @property
-    def report(self) -> pd.DataFrame:
-        df = pd.read_csv(self.issue_count_csv_url)
-        df = df.loc[df["split"] == "all"].drop("split", axis=1).reset_index(drop=True)
-        
-        # Calculate the total sum per column
-        total_count = df['count'].sum()
-        total_pct = df['pct'].sum()
-
-        # Create a DataFrame for the new row and concatenate it with the old DataFrame
-        new_row = pd.DataFrame({'reason': ['Total'], 'count': [total_count], 'pct': [total_pct]})
-        df = pd.concat([df, new_row], ignore_index=True)
-
-        return df
-    
-    def explore(self) -> pd.DataFrame:
-        import base64
-        from itables import init_notebook_mode
-        init_notebook_mode(all_interactive=True)
-
-        def to_img_tag(path):
-            if isinstance(path, str):
-                with open(path, 'rb') as f:
-                    image_data = f.read()
-                    base64_image = base64.b64encode(image_data).decode('utf-8')
-                return '<img src="data:image/png;base64,' + base64_image + '" width="150" >'
-            else:
-                return path  # Return the original value if it's not a string
-
-
-        df = pd.read_csv(self.filelist_csv_url)
-        df["filename_preview"] = df["filename"]
-        df["prototype_preview"] = df["prototype"]
-        df = df.loc[
-            :,
-            [
-                "filename",
-                "filename_preview",
-                "reason",
-                "value",
-                "prototype",
-                "prototype_preview",
-            ],
-        ]
-        df["filename_preview"] = df["filename"].apply(to_img_tag)
-        df["prototype_preview"] = df["prototype"].apply(to_img_tag)
-
-        return df
     
     def export(
         self,
@@ -131,7 +61,3 @@ class VLFood101:
             raise ValueError(
                 f"Unknown output format: {output_format} or variant {variant}."
             )
-
-    def export_issues(self, filename: str) -> None:
-        df = pd.read_csv(self.issue_count_csv_url)
-        df.to_csv(filename, index=False)
