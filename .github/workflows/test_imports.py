347b0528366c1073eead4a8ab85f9422c4b892a6
dnth <dickson.neoh@gmail.com>
2023-06-12 10:16:51 +0000

    update import

--- .github/workflows/test_imports.py
+++ .github/workflows/test_imports.py
@@ -1,5 +1,5 @@
 import visuallayer
-from visuallayer.datasets.zoo import VLFood101, VLOxfordIIITPet
+from visuallayer.datasets.zoo import VLFood101, VLOxfordIIITPet, VLImageNet1k
 
 print(f'Running on version: {visuallayer.__version__}')
 print("Successfully imported all modules")
