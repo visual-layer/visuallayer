61d88d5bede6c5a9f0ff3dfe91f6fcce3c6fcf8b
dnth <dickson.neoh@gmail.com>
2023-06-12 19:06:08 +0800

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt', 'r') as fh:
 
 setup(
     name='visuallayer',
-    version='0.0.13',
+    version='0.0.14',
     url='https://github.com/visual-layer/vl-datasets',
     author='Visual Layer',
     author_email='info@visual-layer.com',
