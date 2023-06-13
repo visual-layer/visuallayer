00e18b59d97c9850fdff6606806bbf3b3291ab6e
dnth <dickson.neoh@gmail.com>
2023-06-13 08:44:15 +0000

    add vlkitti to smoke test

--- .github/workflows/test_imports.py
+++ .github/workflows/test_imports.py
@@ -1,5 +1,5 @@
 import visuallayer
-from visuallayer.datasets.zoo import VLFood101, VLOxfordIIITPet, VLImageNet1k, VLImageNet21k
+from visuallayer.datasets.zoo import VLFood101, VLOxfordIIITPet, VLImageNet1k, VLImageNet21k, VLKitti
 
 print(f'Running on version: {visuallayer.__version__}')
 print("Successfully imported all modules")
