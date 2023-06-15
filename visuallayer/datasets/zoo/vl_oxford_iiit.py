b49cbe29910c80448dce9216d3d9f5dd370f24ea
dnth <dickson.neoh@gmail.com>
2023-06-15 16:37:26 +0800

    remove redundant code

--- visuallayer/datasets/zoo/vl_oxford_iiit.py
+++ visuallayer/datasets/zoo/vl_oxford_iiit.py
@@ -15,24 +15,23 @@ class VLOxfordIIITPet(Dataset):
     num_images: int = 7349
     filelist_csv_url: str = "https://sharedvisuallayer.s3.us-east-2.amazonaws.com/visual-layer-sdk/oxford-iiit-pet_images_issue_file_list.csv"
     issue_count_csv_url: str = "https://sharedvisuallayer.s3.us-east-2.amazonaws.com/visual-layer-sdk/oxford-iiit-pet_images_issue_count.csv"
-    exclude_csv: str = None
 
     # Hack: Download the dataset in the current dir
     def __post_init__(self):
         OxfordIIITPet(root=self.root, download=True)
-
+    
     def export(
         self,
         output_format: str,
         variation: str = "vl",
-        split: str = "train",
+        split: str = "trainval",
     ):
         if output_format == "pytorch":
             if variation == "vl":
                 print(
                     f"Exporting {variation.upper()} dataset into {output_format} dataset."
                 )
-                return CleanTorchvisionOxfordIIITPet(root=self.root, split=split, exclude_csv=self.exclude_csv)
+                return CleanTorchvisionOxfordIIITPet(root=self.root, split=split, exclude_csv=self.filelist_csv_url)
             elif variation == "original":
                 print(
                     f"Exporting {variation.upper()} dataset into {output_format} dataset."
@@ -44,7 +43,7 @@ class VLOxfordIIITPet(Dataset):
                 print(
                     f"Exporting {variation.upper()} dataset into {output_format} dataset."
                 )
-                dataset = CleanTorchvisionOxfordIIITPet(root=self.root, split=split, exclude_csv=self.exclude_csv)
+                dataset = CleanTorchvisionOxfordIIITPet(root=self.root, split=split)
                 samples = {"Image": dataset._images, "Label": dataset._labels}
                 df = pd.DataFrame(samples)
                 return df
